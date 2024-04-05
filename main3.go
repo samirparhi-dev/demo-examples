83a576fd78102941def1153ea9135b994f03c0da
samirparhi-dev <samirparhi@gmail.com>
2024-04-05 11:47:39 +0530

    modified changes

--- main3.go
+++ main3.go
@@ -1,39 +1,53 @@
 package main
 
 import (
-    "fmt"
-    "database/sql"
-    _ "github.com/go-sql-driver/mysql"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func main() {
-    var userInput string
+	// Initialize the database connection
+	initDB()
+	defer db.Close()
 
-    // Taking user input
-    fmt.Print("Enter your username: ")
-    fmt.Scanln(&userInput)
+	// Handle HTTP requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Extract the user input (unsafe)
+		query := r.URL.Query().Get("query")
 
-    // Simulating SQL query
-    query := fmt.Sprintf("SELECT * FROM users WHERE username='%s'", userInput)
+		// Query the database (vulnerable to SQL injection)
+		rows, err := db.Query("SELECT * FROM users WHERE username='" + query + "'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
 
-    // Executing SQL query (This is vulnerable to SQL injection)
-    db, err := sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/database")
-    if err != nil {
- 	   panic(err.Error())
-    }
-    defer db.Close()
+		// Print the results
+		for rows.Next() {
+			var id int
+			var username string
+			err := rows.Scan(&id, &username)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Fprintf(w, "ID: %d, Username: %s\n", id, username)
+		}
+	})
 
-    rows, err := db.Query(query)
-    if err != nil {
- 	   panic(err.Error())
-    }
-    defer rows.Close()
+	// Start the HTTP server
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-    // Displaying results
-    for rows.Next() {
- 	   var id int
- 	   var username string
- 	   rows.Scan(&id, &username)
- 	   fmt.Printf("ID: %d, Username: %s\n", id, username)
-    }
+func initDB() {
+	var err error
+	db, err = sql.Open("mysql", "user:password@tcp(127.0.0.1:3306)/database")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
